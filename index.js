ee46e78d67ae59c27501905fab42763969f8c40d
Ruslan <149687540+nebyvv@users.noreply.github.com>
2024-12-04 03:57:20 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+//////////////////////////////////TASK1/////////////////////
+// const input = document.getElementById("nameInput");
+// input.addEventListener("input", (event) => {
+//   const value = event.target.value;
+//   let formatValue = "";
+
+//   for (let i = 0; i < value.length; i++) {
+//     const char = value[i];
+//     if (!(char >= "0" && char <= "9")) {
+//       formatValue += char;
+//     }
+//   }
+//   event.target.value = formatValue;
+// });
+
+////////////////////////////////////////////////////
+//////////////TASK2//////////////////
+// const openButton = document.getElementById("openButton");
+// const closeButton = document.getElementById("closeButton");
+// const modal = document.getElementById("modal");
+
+// function openModal() {
+//   modal.style.display = "flex";
+// }
+// function closeModal() {
+//   modal.style.display = "none";
+// }
+// openButton.addEventListener("click", openModal);
+// closeButton.addEventListener("click", closeModal);
+
+///////////////////////////////////////////////////////////
+//////////////////////////TASK4
+// const light = document.querySelectorAll(".light");
+// const nextBtn = document.getElementById("next-button");
+// let index = 0;
+
+// nextBtn.addEventListener("click", () => {
+//   light[index].classList.remove("active");
+
+//   if (index == light.length - 1) {
+//     index = 0;
+//   } else {
+//     index += 1;
+//   }
+
+//   light[index].classList.add("active");
+// });
+///////////////////////////////////////////////
+//////////////
+//////////////////TASK6
